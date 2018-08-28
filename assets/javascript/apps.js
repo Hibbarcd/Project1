269b5947af385dffa7daad8c4b59fb617b4ce404
Chris Gudmundsson <chrisgud85@gmail.com>
2018-08-28 15:50:58 -0500

    fixed URI issue; added result for no results; fixed duplicate buttons

--- assets/javascript/apps.js
+++ assets/javascript/apps.js
@@ -1,5 +1,4 @@
-//Firebase Configuration
-var config = {
+var config = { //Firebase Configuration Information
     apiKey: "AIzaSyD__E2ZYbHa1WJAGNzsV9hxjndLC_uxTdY",
     authDomain: "news-and-views-92a23.firebaseapp.com",
     databaseURL: "https://news-and-views-92a23.firebaseio.com",
@@ -9,17 +8,26 @@ var config = {
 };
 firebase.initializeApp(config);
 var database = firebase.database();
+/*
+ *  Once on load and when any value in the database changes:
+ *  1. Create an array of strings containing the last 10 unique queries stored
+ *  2. Translate those strings into new button elements displayed to the page in the #buttons Div
+ */
 database.ref('/users').orderByChild("dateAdded").on("value", function (snapshot) {
 
-    var data = [];
+    var data = []; //array to translate snapshot
 
     snapshot.forEach(ss => {
-        data.push(ss.val());
+        if (!data.includes(ss.val())) { data.push(ss.val()); } //If unique value add to data array
     })
 
+    /*
+     *  Creates buttons from array of data
+     */
     function buttonCreation() {
+        $("#buttons").empty();
         for (i = data.length - 1; i > data.length - 11; i--) {
-            if(data[i].length > 0) {
+            if (data[i].length > 0) {
                 var searchTermBtn = $("<button>");
                 searchTermBtn.addClass("recentSearchButton btn btn-secondary m-2");
                 searchTermBtn.attr("value", data[i]);
@@ -29,13 +37,7 @@ database.ref('/users').orderByChild("dateAdded").on("value", function (snapshot)
         }
     }
 
-    $("#buttons").empty();
     buttonCreation();
-
-    $('#submitButton').on('click', function (event) {
-        $("#buttons").empty();
-        buttonCreation();
-    });
 
     $(".recentSearchButton").on('click', function () {
         var recentSearchTerm = $(this).attr('value');
@@ -45,25 +47,30 @@ database.ref('/users').orderByChild("dateAdded").on("value", function (snapshot)
 /*
  * Click event listener for submitButton
  * uses submitInput for API calls to construct and display article+video pairs
-*/
+ */
 $('#submitButton').on('click', async function () {
     event.preventDefault();
     $('#contentDiv').empty(); // Remove existing results if there are any
     const submitQuery = encodeURI($('#submitInput').val().trim()); // Eventually need INPUT VALIDATION!
     if (submitQuery.length > 0 && submitQuery.length < 256) { searchAPIs(submitQuery) }
-    else { $('#contentDiv').html('<h1>Search Query must not be empty or longer than 256 letters!'); }
+    else $('#contentDiv').html('<h1>Search Query must not be empty or longer than 256 letters!');
 });
+/*
+ *  searchAPIs takes a query string and conducts a series of AJAX requests
+ */
 async function searchAPIs(query) {
-    database.ref('/users').push(encodeURI(query));
+    database.ref('/users').push(decodeURI(query));
     const queryURL = `https://newsapi.org/v2/everything?q=${query}&sources=cnn,abc-news&sortBy=popularity&language=en&apiKey=d63c8717380a49a38ca6816cd34124b4`;
     const res = await $.get(queryURL); //Pause execution until result
-    for (let i = 0; i < res.articles.length; i++) {
-        const article = res.articles[i]; // Get each article from response object
-        requestVideo(article.title)  // Each call awaits a result before advancing
-            .then((video) => {
-                appendDiv(article, video); // Displays article+video result in contentDiv
-            });
-    }
+    if (res.articles.length !== 0) {
+        for (let i = 0; i < res.articles.length; i++) {
+            const article = res.articles[i]; // Get each article from response object
+            requestVideo(article.title)  // Each call awaits a result before advancing
+                .then((video) => {
+                    appendDiv(article, video); // Displays article+video result in contentDiv
+                });
+        }
+    } else $('#contentDiv').html(`<h1>There were no results found for ${query}!</h1>`);
 }
 /*
  * method breakout to carry article information through AJAX request
@@ -77,10 +84,12 @@ async function requestVideo(title) {
  */
 function appendDiv(article, video) {
 
+    //Extract publishing information
     const day = article.publishedAt.substring(8, 10);
     const month = article.publishedAt.substring(5, 7);
     const year = article.publishedAt.substring(0, 4);
 
+    //Construct article+video content for HTML
     $('#contentDiv').append(`
                     <div class='row mb-3'>
                         <div class='col-xl-6 my-2'>
@@ -102,6 +111,9 @@ function appendDiv(article, video) {
                     </div>
                     `);
 }
+/*
+ *  When 'Random' button is clicked, perform a search using a random word from the defined list
+ */
 $("#randomButton").on('click', function () {
     var notReallyRandomWord = ['Caustic', 'Dominica', 'Genethlialogy', 'White House', 'Disasters', 'Epic', 'Football', 'Children', 'Mexico', 'Russia', 'Florida',];
     var randomWord = notReallyRandomWord[Math.floor(Math.random() * notReallyRandomWord.length)];
